29c909b9f98d172b4425c2cad56ea9737631f685
Logassi <lomonagagaspas@gmail.com>
2024-09-02 17:22:55 +0700

    feat : Adding isItPalindrome

--- exercise_3.ts
+++ exercise_3.ts
@@ -5,5 +5,33 @@ function displayMultiplicationTable(theNumber : number){
     }
 }
 
-displayMultiplicationTable(9);
+// displayMultiplicationTable(9);
 
+// -------------------------- exercise check palindrome
+
+
+function isItPalindrome(theString : string){
+    let pointingBegining : number  = 0;
+    let pointingEnd : number = theString.length-1; 
+
+    // for(let i : number = 0; i < theString.length; i++){
+    //     if(theString[i] !== theString[pointingEnd]){
+    //         return "The String is Not Palindrome";
+    //     }
+    //     pointingEnd--;
+    // }
+
+    while (pointingBegining < pointingEnd) {
+        if (theString[pointingBegining] !== theString[pointingEnd]) {
+            return "The String is Not Palindrome";
+        }
+        pointingBegining++;
+        pointingEnd--;
+    }
+    
+    return "The String IS Palindrome";
+}
+
+console.log(isItPalindrome("hesoseh"));
+console.log(isItPalindrome("madam"));
+console.log(isItPalindrome("aezakmi"));
