e85e463e6785ba090c6f76923f8bfa302d642092
Logassi <lomonagagaspas@gmail.com>
2024-09-02 13:12:31 +0700

    feat : Adding isItPrimeNumber

--- exercise_2.ts
+++ exercise_2.ts
@@ -13,3 +13,32 @@ function isItOddOrEven(theNumber : number){
 console.log(isItOddOrEven(25));
 console.log(isItOddOrEven(2));
 console.log(isItOddOrEven(-22));
+
+// -------------------------- exercise check whether prime number
+function isItPrimeNumber(theNumber : number){
+    if(theNumber < 1){
+        return "The Number is not Prime";
+    }
+
+    for(let i : number = 2; i < theNumber; i++){ // im thinking two pointer would be faster
+        if(theNumber % i === 0){
+            return "The Number is NOT Prime";
+        }
+    }
+
+    return "The Number is Prime";
+}
+
+console.log(isItPrimeNumber(-3));
+console.log(isItPrimeNumber(1));
+console.log(isItPrimeNumber(2));
+console.log(isItPrimeNumber(3));
+console.log(isItPrimeNumber(4));
+console.log(isItPrimeNumber(5));
+console.log(isItPrimeNumber(6));
+console.log(isItPrimeNumber(7));
+console.log(isItPrimeNumber(8));
+console.log(isItPrimeNumber(9));
+console.log(isItPrimeNumber(10));
+
+
