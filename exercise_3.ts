8fd3768202a210b948df4358a5a65e66695d7135
Logassi <lomonagagaspas@gmail.com>
2024-09-02 14:17:54 +0700

    feat : Initiating exercise_3.ts and Adding displayMultiplicationTable

---
+++ exercise_3.ts
@@ -0,0 +1,9 @@
+// -------------------------- exercise display the multiplication table
+function displayMultiplicationTable(theNumber : number){
+    for(let i : number = 1; i <= 10; i++){
+        console.log(theNumber + " x " + i + " = " + theNumber*i);
+    }
+}
+
+displayMultiplicationTable(9);
+
