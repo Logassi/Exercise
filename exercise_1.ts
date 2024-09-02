40dd846c97d571f25abfa867595dd6598b32b2bb
Logassi <lomonagagaspas@gmail.com>
2024-09-02 09:57:28 +0700

    feat : Adding convertDaysToYearsMonthsDays, and getDifferenceInDays

--- exercise_1.ts
+++ exercise_1.ts
@@ -35,7 +35,6 @@ console.log(findCircumference(radius));
 console.log(findAreaOfCircle(radius));
 
 // -------------------------- exercise find angle of triangle if angles are given
-
 function findAnglesOfTriangle(angleA : number, angleB : number){
     return 180 - (angleA+angleB);
 }
@@ -43,3 +42,49 @@ function findAnglesOfTriangle(angleA : number, angleB : number){
 let theAngleA : number = 80;
 let theAngleB : number = 65;
 console.log(findAnglesOfTriangle(theAngleA, theAngleB));
+
+// -------------------------- exercise convert days to years (Notes: 1 year : 365 days, 1 month : 30days).
+function convertDaysToYearsMonthsDays(totalDays: number) {
+    const daysInYear = 365;
+    const daysInMonth = 30;
+
+    const years = Math.floor(totalDays / daysInYear);
+    const remainingDaysAfterYears = totalDays % daysInYear;
+
+    const months = Math.floor(remainingDaysAfterYears / daysInMonth);
+    const remainingDays = remainingDaysAfterYears % daysInMonth;
+
+    return {
+        years: years,
+        months: months,
+        days: remainingDays
+    };
+}
+
+let totalDays1 = 400;
+let result1 = convertDaysToYearsMonthsDays(totalDays1);
+console.log(`${totalDays1} days → ${result1.years} year(s), ${result1.months} month(s), ${result1.days} day(s)`);
+
+let totalDays2 = 366;
+let result2 = convertDaysToYearsMonthsDays(totalDays2);
+console.log(`${totalDays2} days → ${result2.years} year(s), ${result2.months} month(s), ${result2.days} day(s)`);
+
+
+// -------------------------- exercise get difference between 2 dates
+
+function getDifferenceInDays(date1: string, date2: string): number {
+    const startDate = new Date(date1);
+    const endDate = new Date(date2);
+
+    const differenceInTime = endDate.getTime() - startDate.getTime();
+
+    const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+
+    return differenceInDays;
+}
+
+const date1 = "2022-01-20";
+const date2 = "2022-01-22";
+const daysDifference = getDifferenceInDays(date1, date2);
+
+console.log(daysDifference);
